2b31b788b7e1103a651569bf049ffb7288a621ca
ArnavMeh <mehraarn000@gmail.com>
2024-12-29 22:43:35 -0500

    interval highlighting

--- js-code/index.js
+++ js-code/index.js
@@ -1,9 +1,5 @@
-import { Valuation } from './valuation.js';
+import { PlottedValuation, Valuation } from './valuation.js';
 import { range } from './util.js';
-
-const gen_agents = (n, detail) => {
-    return range(1, n).map(_ => new Valuation(detail));
-};
 
 const allocate_proportionally = (V) => {
     const n = V.length;
@@ -17,21 +13,69 @@ const allocate_proportionally = (V) => {
             throw new Error("Not enough cake left! This should never happen!");
         }
 
-        allocs[j] = [curr_start, mark];
+        allocs[j] = [[curr_start, mark]];
         curr_start = mark;
     }
-    const residue = [curr_start, 1];
+    const residue = [[curr_start, 1]];
 
     return [allocs, residue];
 };
 
+const n = 5;
+const detail = 10;
+const scale_x = 120;
+const scale_y = 30;
 
-const agents = gen_agents(5, 10);
-// console.log(agents);
-const [alloc, residue] = allocate_proportionally(agents);
-console.log(alloc);
+const agents = range(1, n).map(_ => 
+    new PlottedValuation(document.body, 'red', scale_x, scale_y, detail)
+);
+console.log(agents)
+const [allocs, residue] = allocate_proportionally(agents);
+console.log(allocs);
 console.log(residue);
 
+Object.entries(allocs).forEach(([i, intervals]) => {
+    intervals.forEach(interval => {
+        console.log("highlighting " , i , interval)
+        agents[i].highlight(interval, 'green');
+    })
+});
+
+range(0, n - 1).forEach(i => {
+    residue.forEach(interval => {
+        console.log("highlighting " , i , interval)
+        agents[i].highlight(interval, 'gray');
+    })
+});
+
+// const plots = agents.map((v, i) => {
+//     const cont = document.createElement('div');
+//     const base_plot = v.gen_plot('red', scale_x, scale_y);
+//     const alloc_overlays = allocs[i].map(interval => (
+//         v.gen_plot_interval(interval, 'green', scale_x, scale_y)
+//     ));
+//     const layers = [ base_plot, ...alloc_overlays ];
+//     cont.append(...layers);
+
+//     cont.style.position = 'relative';
+//     cont.style.width = scale_x + 'px';
+//     cont.style.height = scale_y * 4 + 'px';
+//     cont.style.overflow = 'hidden';
+//     cont.style.border = '1px solid black';
+//     layers.forEach(layer => {
+//         layer.style.position = 'absolute';
+//         layer.style.bottom = '0px';
+//         layer.style.left = '0px';
+//     });
+//     alloc_overlays.forEach(overlay => {
+//         overlay.style.zIndex = 10;
+//     });
+
+//     return cont;
+// });
+
+// console.log(plots)
+// document.body.append(...plots);
 
 // console.log(v.segments)
 // console.log(v.values)
--- js-code/valuation.js
+++ js-code/valuation.js
@@ -1,16 +1,26 @@
 import { gen_rands } from './util.js';
 
+const create_block = (color, width, height) => {
+    const block = document.createElement("div");
+    block.style.backgroundColor = color;
+    block.style.width = width + "px";
+    block.style.height = height + "px";
+    return block;
+}
+
 export class Valuation {
     constructor(detail) {
         this.segments = gen_rands(detail - 1)
             .sort()
             .pushed(1)
-            .map((x, i, arr) => ([ i === 0 ? 0 : arr[i - 1], x ]));
+            // .map((x, i, arr) => ([ i === 0 ? 0 : arr[i - 1], x ]));
+            .map((_, i) => ([ i * 1 / detail, (i + 1) * 1 / detail ]));
         this.values = gen_rands(detail - 1)
             .sort()
             .pushed(1)
             .map((x, i, arr) => i === 0 ? x : x - arr[i - 1]);
-        this.cum_values = this.values.accumulateLeft();
+            // .map(_ => 1 / detail);
+        // this.cum_values = this.values.accumulateLeft();
         this.queries = 0;
     }
 
@@ -52,13 +62,92 @@ export class Valuation {
         this.queries++;
         return this._mark(start, val);
     }
+};
 
-    gen_plot(color) {
+export class PlottedValuation extends Valuation {
+    constructor(parent, color, scale_x, scale_y, detail) {
+        super(detail);
+
+        this.scale_x = scale_x;
+        this.scale_y = scale_y;
+        this.parent = parent;
+
+        this.cont = document.createElement('div');
+        this.cont.style.position = 'relative';
+        this.cont.style.width = this.scale_x + 'px';
+        this.cont.style.height = this.scale_y * 4 + 'px';
+        this.cont.style.overflow = 'hidden';
+        this.cont.style.border = '1px solid black';
+
+        const base_plot = this._gen_plot(color, this.scale_x, this.scale_y);
+        this.cont.append(base_plot);
+        this.parent.append(this.cont);
+    }
+
+    highlight(interval, color) {
+        const highlight_overlay = this._gen_plot_interval(interval, color, this.scale_x, this.scale_y)
+        highlight_overlay.style.zIndex = 10;
+        this.cont.append(highlight_overlay);
+        return () => highlight_overlay.destroy();
+    }
+
+    _gen_plot_interval([start, end], color='green', scale_x=10, scale_y=10) {
         const cont = document.createElement("div");
         cont.style.display = 'flex';
+        cont.style.alignItems = 'end';
+        cont.style.position = 'absolute';
+        cont.style.bottom = '0px';
+        cont.style.left = '0px';
 
         for (let i = 0; i < this.segments.length; i++) {
-            const cont = document.createElement("div");
+            const [ seg_start, seg_end ] = this.segments[i];
+            const seg_len = seg_end - seg_start;
+            const seg_val = this.values[i];
+
+            const width = seg_len * scale_x;
+            const height = seg_val / seg_len * scale_y
+
+            if (start >= seg_end || end <= seg_start) { // [] start end OR start end []
+                const flat = create_block(color, width, 0);
+                cont.appendChild(flat);
+            }
+            else if (start <= seg_start && end >= seg_end) {  // start [ ] end
+                const bar = create_block(color, width, height);
+                cont.appendChild(bar);
+            }
+            else if (start <= seg_start && end < seg_end) { // start ([ end) ] 
+                const width_f = (seg_end - end) / (seg_end - seg_start);
+                
+                const bar = create_block(color, width * (1 - width_f), height);
+                cont.appendChild(bar);
+                const flat = create_block(color, width * width_f, 0);
+                cont.appendChild(flat);
+            }
+            else if (start > seg_start && end >= seg_end) { // [ (start ]) end 
+                const width_f = (start - seg_start) / (seg_end - seg_start);
+                
+                const flat = create_block(color, width * width_f, 0);
+                cont.appendChild(flat);
+                const bar = create_block(color, width * (1 - width_f), height);
+                cont.appendChild(bar);
+            }
+            else { // [ (start, end) ]
+                const width_f1 = (start - seg_start) / (seg_end - seg_start);
+                const width_f2 = (seg_end - end) / (seg_end - seg_start);
+
+                const flat1 = create_block(color, width * width_f1, 0);
+                cont.appendChild(flat1);
+                const bar = create_block(color, width * (1 - width_f1 - width_f2), height);
+                cont.appendChild(bar);
+                const flat2 = create_block(color, width * width_f2, 0);
+                cont.appendChild(flat2);
+            }
         }
+
+        return cont;
     }
-};
+
+    _gen_plot(color='red', scale_x=10, scale_y=10) {
+        return this._gen_plot_interval([0, 1], color, scale_x, scale_y);
+    }
+}
