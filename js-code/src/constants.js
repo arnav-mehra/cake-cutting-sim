ed75cba7f68b943ab843a225df9c05070b266e90
ArnavMeh <mehraarn000@gmail.com>
2025-01-02 01:17:26 -0500

    stats, curr_instr

--- js-code/src/constants.js
+++ js-code/src/constants.js
@@ -11,15 +11,25 @@ export const unalloc_color = 'gray';
 export const query_color = 'blue';
 export const cut_color = 'black';
 
-export const n_input = document.getElementById('n-input');
-export const code_input = document.getElementById('code-input');
-export const visual_cont = document.getElementById('visual-cont');
-export const run_code_button = document.getElementById('run-code-button');
-export const regen_vals_button = document.getElementById('regen-vals-button');
-export const code_highlight = document.getElementById('code-highlight');
+
 export const title = document.getElementById('title');
 export const panels = document.getElementById('panels');
+
 export const editor_panel = document.getElementById('editor-panel');
+export const n_input = document.getElementById('n-input');
+export const code_input = document.getElementById('code-input');
+export const regen_vals_button = document.getElementById('regen-vals-button');
+export const run_code_button = document.getElementById('run-code-button');
+export const code_highlight = document.getElementById('code-highlight');
+
 export const panel_divider = document.getElementById('panel-divider');
+
+export const curr_instr_el = document.getElementById('current-instruction');
 export const visual_panel = document.getElementById('visual-panel');
+export const visual_cont = document.getElementById('visual-cont');
 export const val_code_input = document.getElementById('valuation-code-input');
+export const cut_count_el = document.getElementById('cut-count');
+export const query_count_el = document.getElementById('query-count');
+export const mark_count_el = document.getElementById('mark-count');
+export const eval_count_el = document.getElementById('eval-count');
+export const residue_set_el = document.getElementById('residue-set');
--- js-code/src/log.js
+++ js-code/src/log.js
@@ -1,5 +1,5 @@
 import { range, sleep } from "./util.js";
-import { alloc_color, animation_query_delay, animation_step_delay, code_highlight, cut_color, query_color, unalloc_color } from "./constants.js";
+import { alloc_color, animation_query_delay, animation_step_delay, code_highlight, curr_instr_el, cut_color, cut_count_el, eval_count_el, mark_count_el, query_color, query_count_el, residue_set_el, unalloc_color } from "./constants.js";
 import { V } from "./cake_vars.js";
 
 export const log = [];
@@ -32,25 +32,57 @@ export const clear_log = () => {
 
 let hanging_overlays = [];
 export const animate_log = async (n) => {
-    hanging_overlays.forEach(cleanup => {
-        cleanup();
-    });
+    code_highlight.classList.remove('hidden');
+    hanging_overlays.forEach(cleanup => cleanup());
 
+    const curr_cuts = [];
     const A_curr = Object.fromEntries(range(1, n).map(i => [i, []]));
     const A_highlights = [];
 
-    code_highlight.classList.remove('hidden');
+    let cuts = 0;
+    let marks = 0;
+    let evals = 0;
+    let curr_instr = 'None';
+
+    const update_stats = () => {
+        cut_count_el.textContent = cuts + '';
+        mark_count_el.textContent = marks + '';
+        eval_count_el.textContent = evals + '';
+        query_count_el.textContent = marks + evals + '';
+        curr_instr_el.textContent = curr_instr;
+   
+        const intervals_starts = [ 0, ...curr_cuts ];
+        const intervals_ends = [ ...curr_cuts, 1 ];
+        const curr_intervals = intervals_starts.zipWith(intervals_ends);
+        const alloc_intervals = range(1, n).map(i => A_curr[i]).flat().map(int => [int[0], int[1]]);
+        const intervals_str_set = new Set(curr_intervals.map(x => x.join(',')));
+        const alloc_intervals_str_set = new Set(alloc_intervals.map(x => x.join(',')));
+        const unalloc_intervals_str_set = intervals_str_set.subtract(...alloc_intervals_str_set);
+        const unalloc_intervals_str = unalloc_intervals_str_set.map(x => "(" + x.split(',').map(x => Number(x).toFixed(2)).join(', ') + ")")
+        residue_set_el.textContent = "{ " + unalloc_intervals_str.join(', ') + " }";
+    };
+    update_stats();
 
     for (const [cmd, ...io] of log) {
         switch (cmd) {
             case 'exec': {
                 const [ line_num ] = io;
+
+                curr_instr = `None`;
+                update_stats();
+
                 code_highlight.style.top = line_num * 24 + 'px';
                 await sleep(animation_step_delay);
                 break;
             }
             case 'eval': {
                 const [ v, start, end, res ] = io;
+
+                evals++;
+                const i = Object.entries(V).find(([_, _v]) => _v === v)[0];
+                curr_instr = `V[${i}].eval(${start.toFixed(2)}, ${end.toFixed(2)}) -> ${res.toFixed(2)}`;
+                update_stats();
+
                 const cleanup = v.highlight([start, end], query_color);
                 await sleep(animation_query_delay);
                 cleanup();
@@ -58,6 +90,12 @@ export const animate_log = async (n) => {
             }
             case 'mark': {
                 const [ v, start, val, res ] = io;
+
+                marks++;
+                const i = Object.entries(V).find(([_, _v]) => _v === v)[0];
+                curr_instr = `V[${i}].mark(${start.toFixed(2)}, ${val.toFixed(2)}) -> ${res.toFixed(2)}`;
+                update_stats();
+
                 const cleanup = v.highlight([start, res], query_color);
                 await sleep(animation_query_delay);
                 cleanup();
@@ -66,6 +104,8 @@ export const animate_log = async (n) => {
             case 'set-A': {
                 const [ i, intervals ] = io;
                 A_curr[i] = intervals;
+
+                update_stats();
 
                 A_highlights.forEach(cleanup => cleanup());
                 A_highlights.length = 0;
@@ -89,6 +129,12 @@ export const animate_log = async (n) => {
             }
             case 'cut': {
                 const [ marks ] = io;
+                curr_cuts.push(...marks);
+
+                cuts += marks.length;
+                curr_instr = `cut(${marks.map(x => x.toFixed(2)).join(', ')})`;
+                update_stats();
+
                 range(1, n).forEach(i => {
                     marks.forEach(mark => {
                         const cut_cleanup = V[i].show_cut(mark, cut_color)
